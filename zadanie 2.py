ee54fbdd55cd18c7035258d3939a1e2bcee243d7
Lorenzo <ratedrko@yandex.ru>
2022-02-15 15:34:14 +0300

    Исправлено

--- zadanie 2.py
+++ zadanie 2.py
@@ -10,30 +10,40 @@ pattern_email = r'(\w+\@\w+\.\w+)'
 pattern_site = r'(\w+.\w+.(\w+?))'
 
 
-def dropper(func):
+def dropper(func, drop_incorrect):
     def wrapper(*args, **kwargs):
-        for user in func(*args, **kwargs):
-            if re.fullmatch(pattern_name, user['name']) is None:
-                # user['name'] = 'None'
-                continue
-            if re.fullmatch(pattern_surname, user['surname']) is None:
-                # user['surname'] = 'None'
-                continue
-            if re.fullmatch(pattern_sex, user['sex']) is None:
-                # user['sex'] = 'None'
-                continue
-            if type(user['age']) != int or (int(user['age']) < 18 or int(user['age']) > 99):
-                # user['age'] = 'None'
-                continue
-            if user['contacts']['tel'] is None or re.fullmatch(pattern_tel, user['contacts']['tel']) is not None:
-                # user['contacts']['tel'] = 'None'
-                continue
-            if user['contacts']['email'] is None or re.fullmatch(pattern_email, user['contacts']['email']) is not None:
-                # user['contacts']['email'] = 'None'
-                continue
-            if user['contacts']['site'] is None or re.fullmatch(pattern_site, user['contacts']['site']) is not None:
-                # user['contacts']['site'] = 'None'
-                continue
+        if drop_incorrect is True:
+            for user in func(*args, **kwargs):
+                if re.fullmatch(pattern_name, user['name']) is None:
+                    continue
+                if re.fullmatch(pattern_surname, user['surname']) is None:
+                    continue
+                if re.fullmatch(pattern_sex, user['sex']) is None:
+                    continue
+                if type(user['age']) != int or (int(user['age']) < 18 or int(user['age']) > 99):
+                    continue
+                if user['contacts']['tel'] is None or re.fullmatch(pattern_tel, user['contacts']['tel']) is not None:
+                    continue
+                if user['contacts']['email'] is None or re.fullmatch(pattern_email, user['contacts']['email']) is not None:
+                    continue
+                if user['contacts']['site'] is None or re.fullmatch(pattern_site, user['contacts']['site']) is not None:
+                    continue
+        else:
+            for user in func(*args, **kwargs):
+                if re.fullmatch(pattern_name, user['name']) is None:
+                    user['name'] = 'None'
+                if re.fullmatch(pattern_surname, user['surname']) is None:
+                    user['surname'] = 'None'
+                if re.fullmatch(pattern_sex, user['sex']) is None:
+                    user['sex'] = 'None'
+                if type(user['age']) != int or (int(user['age']) < 18 or int(user['age']) > 99):
+                    user['age'] = 'None'
+                if user['contacts']['tel'] is None or re.fullmatch(pattern_tel, user['contacts']['tel']) is not None:
+                    user['contacts']['tel'] = 'None'
+                if user['contacts']['email'] is None or re.fullmatch(pattern_email, user['contacts']['email']) is not None:
+                    user['contacts']['email'] = 'None'
+                if user['contacts']['site'] is None or re.fullmatch(pattern_site, user['contacts']['site']) is not None:
+                    user['contacts']['site'] = 'None'
             yield user
 
     return wrapper
@@ -48,5 +58,5 @@ def user_generator(file_name: str) -> Iterable[Dict]:
 
 file_name = "users_2240.json"
 
-for user_ in dropper(user_generator)(file_name):
+for user_ in dropper(user_generator)(file_name, drop_incorrect=False):
     print(user_)
